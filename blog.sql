49d10e85615e3e1f3788fee28d1d7425a592eced
henry <henrychenchn@gmail.com>
2020-09-07 22:02:16 +0800

    ...

--- blog.sql
+++ blog.sql
@@ -1,4 +1,5 @@
+-- username:henry
+-- password:123.com
+
 use blog;
 db.user.insert({username:"henry",password:"$2a$10$8hVyEv5p3rnHnE8L0TkBZeOwG7Vsa1KF/QOMJdMqSYgV2ncFqfBVe"});
-
-# password:123.com
